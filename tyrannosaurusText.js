da86489f2cc00b9b19e0b344c901f834135b00d7
pjflanagan <flanapet@umich.edu>
2015-04-02 15:50:51 -0400

    Text width works
    Vertical alignment still proving to be difficult

--- tyrannosaurusText.js
+++ tyrannosaurusText.js
@@ -1,64 +1,75 @@
 //TyrannosaurusText.js
-
-function Text(class){
+function Text(c){
 
 	//set font to really large by default
 
-	this.width = document.getElementByClassName(class)[0].style.width;
-	this.all_caps = false;
+	this.class = c;
+	this.width = 200//document.getElementsByClassName(this.class)[0].style.width;
+	/*this.all_caps = false;
 	this.avoid_overflow = true; //true by default
 	this.line_height_gap = 2; //2 by default
-	//this.words_per_line = 1 //1 by default
+	this.words_per_line = 1; //1 by default
 	this.alignX = "center";
-	this.height = document.getElementByClassName(class)[0].style.width;
+	//this.height = document.getElementsByClassName(this.class)[0].style.height;
 	this.alignY = "center"; //vertically center by default
-
-	private previous_size,
-			total_height,
-			span_count,
-			heights[];
+	*/
+	
+	var N = 0;
 
 	this.set = function(){
-		//for document.getElementByClass[]
-		//
-		//getString
-		//insertSpansAndBreaks
-		//checkOverflow
-			//total_height++
-		//setMarginTop
-			//total_height++
-		//verticalAlign
-		
+		var len = document.getElementsByClassName(this.class).length;
+
+		for(var i = 0; i<len; i++){
+			string = this.getString(i);
+			sizeable = this.insertP(string).join("");
+			document.getElementsByClassName(this.class)[i].innerHTML = "<div class='TyrannosaurusText'>" + sizeable + "</div>";
+		} 
+		for(var i = 0; i<N; i++){
+			this.setSize(i);
+			this.setMarginTop(i);
+		}
+		return;
 	}
 
+	this.getString = function(i){
+		return document.getElementsByClassName(this.class)[i].innerHTML;
+	}
+
+	this.insertP = function(string){
+		body = string.split(" ");
+		for(var i = 0 ; i<body.length; i++){
+			body[i] = "<span class='Saurus' id='Saurus-" + N + "'>" + body[i] + "</span><br />"
+			N++;
+		}
+		return body;
+	}
+
+	this.setSize = function(id){
+		//if(!avoid_overflow)return;
+		span = document.getElementById("Saurus-"+id);
+		for(var i = 0; span.offsetWidth<this.width; i++){
+			span.style.fontSize = i;
+		}
+		return;
+	}
+
+	this.setMarginTop = function(id){
+		if(id==0)return;
+		span = document.getElementById("Saurus-"+id);
+		span_prev = document.getElementById("Saurus-"+(id-1));
+		prev_height = span_prev.offsetHeight;
+		span.style.top = 0;
+		span.style.marginTop = prev_height;
+	}
+
+	/*
 	function verticalAlign(){
 
 	}
 
-	function setMarginTop(){
 
-	}
-
-	function checkOverflow(line){
-		if(!avoid_overflow)return;
-		//overflow = span.width > width
-		while(overflow){
-			//line_font--;
-		}
-	}
-
-	function getString(){
-
-	}
-
-	function insertSpansAndBreaks(){
-		//string.split(" ")
-		//for(N)
-		//insert <span class="Saurus" id="Saurus-N">
-		//replace ("<br />")
-	}
-
-	//.saurus {position:relative;}
+	/*
+	*/
 }
 
 
